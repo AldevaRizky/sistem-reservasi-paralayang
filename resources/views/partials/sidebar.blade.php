4ca2c8a2355c8b81cc0a73d239d7814b09fe5e45
Aldeva Rizky <aldevarwpp@gmail.com>
2025-06-25 21:36:46 +0700

    refactor(view): convert static sidebar menu into dynamic role-based menu rendering Enhance maintainability and flexibility by: - **Defining sidebar menu configuration via a `$menus` array**, including role-based visibility - **Generating menu and nested submenu items dynamically** using recursive PHP closures - **Using `auth()->user()->role` to determine accessible items per user** - **Switching hardcoded routes and icons to reusable Blade/PHP logic** - **Improving role-based dashboard navigation for admin, staff, and user levels**

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,71 +1,124 @@
+@php
+    $role = auth()->user()->role;
+    $routePrefix = match($role) {
+        'admin' => 'admin.',
+        'staff' => 'staff.',
+        'user' => 'user.',
+        default => '',
+    };
+
+    $menus = [
+        [
+            'type' => 'header',
+            'name' => 'Navigation',
+            'icon' => 'menu',
+            'roles' => ['admin', 'staff', 'user'],
+        ],
+        [
+            'type' => 'menu',
+            'name' => 'Dashboard',
+            'url' => route($routePrefix . 'dashboard'),
+            'icon' => 'home',
+            'roles' => ['admin', 'staff', 'user'],
+        ],
+        [
+            'type' => 'header',
+            'name' => 'Other',
+            'icon' => 'sidebar',
+            'roles' => ['admin'],
+        ],
+        [
+            'type' => 'menu',
+            'name' => 'Menu levels',
+            'url' => '#!',
+            'icon' => 'align-right',
+            'roles' => ['admin'],
+            'submenu' => [
+                [
+                    'name' => 'Level 2.1',
+                    'url' => '#!',
+                ],
+                [
+                    'name' => 'Level 2.2',
+                    'url' => '#!',
+                    'submenu' => [
+                        ['name' => 'Level 3.1', 'url' => '#!'],
+                        ['name' => 'Level 3.2', 'url' => '#!'],
+                        [
+                            'name' => 'Level 3.3',
+                            'url' => '#!',
+                            'submenu' => [
+                                ['name' => 'Level 4.1', 'url' => '#!'],
+                                ['name' => 'Level 4.2', 'url' => '#!'],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    // Rekursif submenu renderer
+    $renderSubmenu = function ($submenu) use (&$renderSubmenu) {
+        foreach ($submenu as $sub) {
+            $hasSub = isset($sub['submenu']);
+            echo '<li class="pc-item' . ($hasSub ? ' pc-hasmenu' : '') . '">';
+            echo '<a href="' . $sub['url'] . '" class="pc-link">' . $sub['name'];
+            if ($hasSub) {
+                echo '<span class="pc-arrow"><i class="ti ti-chevron-right"></i></span>';
+            }
+            echo '</a>';
+            if ($hasSub) {
+                echo '<ul class="pc-submenu">';
+                $renderSubmenu($sub['submenu']);
+                echo '</ul>';
+            }
+            echo '</li>';
+        }
+    };
+
+    // Menu utama renderer
+    $renderMenu = function ($menus) use (&$renderMenu, $renderSubmenu, $role) {
+        foreach ($menus as $menu) {
+            if (!in_array($role, $menu['roles'])) continue;
+
+            if ($menu['type'] === 'header') {
+                echo '<li class="pc-item pc-caption"><label>' . $menu['name'] . '</label>';
+                echo '<i data-feather="' . $menu['icon'] . '"></i></li>';
+            }
+
+            if ($menu['type'] === 'menu') {
+                $hasSub = isset($menu['submenu']);
+                echo '<li class="pc-item' . ($hasSub ? ' pc-hasmenu' : '') . '">';
+                echo '<a href="' . $menu['url'] . '" class="pc-link">';
+                echo '<span class="pc-micon"><i data-feather="' . $menu['icon'] . '"></i></span>';
+                echo '<span class="pc-mtext">' . $menu['name'] . '</span>';
+                if ($hasSub) {
+                    echo '<span class="pc-arrow"><i class="ti ti-chevron-right"></i></span>';
+                }
+                echo '</a>';
+                if ($hasSub) {
+                    echo '<ul class="pc-submenu">';
+                    $renderSubmenu($menu['submenu']);
+                    echo '</ul>';
+                }
+                echo '</li>';
+            }
+        }
+    };
+@endphp
+
 <nav class="pc-sidebar">
     <div class="navbar-wrapper">
         <div class="m-header flex items-center py-4 px-6 h-header-height">
-            <a href="../dashboard/index.html" class="b-brand flex items-center gap-3">
-                <!-- ========   Change your logo from here   ============ -->
-                <img src="../assets/images/logo-white.svg" class="img-fluid logo logo-lg" alt="logo" />
-                <img src="../assets/images/favicon.svg" class="img-fluid logo logo-sm" alt="logo" />
+            <a href="{{ route($routePrefix . 'dashboard') }}" class="b-brand flex items-center gap-3">
+                <img src="{{ asset('assets/images/logo-white.svg') }}" class="img-fluid logo logo-lg" alt="logo" />
+                <img src="{{ asset('assets/images/favicon.svg') }}" class="img-fluid logo logo-sm" alt="logo" />
             </a>
         </div>
         <div class="navbar-content h-[calc(100vh_-_74px)] py-2.5">
             <ul class="pc-navbar">
-                <li class="pc-item pc-caption">
-                    <label>Navigation</label>
-                </li>
-                <li class="pc-item">
-                <li class="pc-item">
-                    <a href="../dashboard/index.html" class="pc-link">
-                        <span class="pc-micon">
-                            <i data-feather="home"></i>
-                        </span>
-                        <span class="pc-mtext">Dashboard</span>
-                    </a>
-                </li>
-
-                <li class="pc-item pc-caption">
-                    <label>Other</label>
-                    <i data-feather="sidebar"></i>
-                </li>
-                <li class="pc-item pc-hasmenu">
-                    <a href="#!" class="pc-link"><span class="pc-micon"> <i data-feather="align-right"></i>
-                        </span><span class="pc-mtext">Menu levels</span><span class="pc-arrow"><i
-                                class="ti ti-chevron-right"></i></span></a>
-                    <ul class="pc-submenu">
-                        <li class="pc-item"><a class="pc-link" href="#!">Level 2.1</a></li>
-                        <li class="pc-item pc-hasmenu">
-                            <a href="#!" class="pc-link">Level 2.2<span class="pc-arrow"><i
-                                        class="ti ti-chevron-right"></i></span></a>
-                            <ul class="pc-submenu">
-                                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                                <li class="pc-item pc-hasmenu">
-                                    <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i
-                                                class="ti ti-chevron-right"></i></span></a>
-                                    <ul class="pc-submenu">
-                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="pc-item pc-hasmenu">
-                            <a href="#!" class="pc-link">Level 2.3<span class="pc-arrow"><i
-                                        class="ti ti-chevron-right"></i></span></a>
-                            <ul class="pc-submenu">
-                                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                                <li class="pc-item pc-hasmenu">
-                                    <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i
-                                                class="ti ti-chevron-right"></i></span></a>
-                                    <ul class="pc-submenu">
-                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
+                @php $renderMenu($menus); @endphp
             </ul>
         </div>
     </div>
